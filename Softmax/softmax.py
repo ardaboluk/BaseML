91bde6d22b30cb33093f04b82eda9da493196b59
ardaboluk <ardaboluk2@gmail.com>
2017-07-24 21:05:49 +0300

    Added confusion matrix, precision-recall curve and roc curve.

--- Softmax/softmax.py
+++ Softmax/softmax.py
@@ -1,10 +1,13 @@
 
 import numpy as np
+import pandas as pd
 from extractMnistMulticlass import readMnist
 from model_selection import StratifiedKFold
 from sklearn import preprocessing
 from sklearn import metrics
 from matplotlib import pyplot as plt
+import seaborn as sn
+import itertools
     
 def trainSoftmax(trainInputs, trainTargets, alpha = 12e-8, maxEpochs = 1000, minCostDiff = 0.001):
     """Trains the softmax regression model with the given training data and returns model parameters.
@@ -62,8 +65,15 @@ np.random.shuffle(s)
 data = data[:, s].astype(float)
 labels = labels[:, s]
 
-# train and test in a k-fold stratified cross-validation setting
+# average accuracy
 avg_acc = 0
+# all of the predictions and true labels for calculating the final metrics
+# we shouldn'd collect the metrics themselves and average them, because in k-fold cross validation,
+# each sample is tested once
+whole_true_labels = None
+whole_predictions = None
+
+# train and test in a k-fold stratified cross-validation setting
 stf = StratifiedKFold(labels, num_folds = numFolds)
 curFold = 1
 for train_indices, test_indices in stf:
@@ -83,15 +93,58 @@ for train_indices, test_indices in stf:
     test_data = np.vstack((test_data, np.ones((test_data.shape[1]))))
 
     # train the softmax algorithm
-    W = trainSoftmax(train_data, train_labels, maxEpochs = 10)
+    W = trainSoftmax(train_data, train_labels, maxEpochs = 100)
 
-    # evaluate and calculate accruracy
+    # evaluate the model
     predictions = W.dot(test_data)
+
+    # collect the predictions and true labels for the final metrics
+    if whole_predictions is None:
+        whole_predictions = predictions
+        whole_true_labels = test_labels
+    else:
+        whole_predictions = np.concatenate((whole_predictions, predictions), axis = 1)
+        whole_true_labels = np.concatenate((whole_true_labels, test_labels), axis = 1)
+
+    # calculate accuracy
     acc = np.sum(np.argmax(test_labels, axis = 0) == np.argmax(predictions, axis = 0)) / test_labels.shape[1]
     avg_acc += acc
     print("Fold {} test accuracy: {}".format(curFold, acc))
     print()
     curFold += 1
 
+# calculate and print average accuracy
 avg_acc /= numFolds
 print("Average accuracy {}".format(avg_acc))
+
+# calculate and plot confusion matrix
+conf_mat = metrics.confusion_matrix(np.argmax(whole_true_labels, axis = 0), np.argmax(whole_predictions, axis = 0))
+df_cm = pd.DataFrame(conf_mat, index = [i for i in range(0, test_labels.shape[0])], columns = [i for i in range(0, test_labels.shape[0])])
+plt.figure()
+sn.heatmap(df_cm, annot = True)
+plt.show()
+
+# calculate and plot precision-recall and roc curves for each class'
+plt.figure(1)
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.title("PR curve for all classes")
+plt.figure(2)
+plt.xlabel("FPR")
+plt.ylabel("TPR")
+plt.title("ROC curve for all classes")
+for curClass, (preds, trues, plot_marker) in enumerate(zip(whole_predictions, whole_true_labels, itertools.cycle(['-','--','-.',':']))):
+
+    cur_precision, cur_recall, _ = metrics.precision_recall_curve(trues, preds)
+    cur_fpr, cur_tpr, _ = metrics.roc_curve(trues, preds)
+
+    plt.figure(1)
+    plt.plot(cur_recall, cur_precision, plot_marker, label = "Class {}".format(curClass))
+    plt.figure(2)
+    plt.plot(cur_fpr, cur_tpr, plot_marker, label = "Class {}".format(curClass))
+
+plt.figure(1)
+plt.legend(loc = 3)
+plt.figure(2)
+plt.legend(loc = 4)
+plt.show()
