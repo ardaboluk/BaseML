ccef60bfa7f991bc27df67937e52a9175417f16d
ardaboluk <ardaboluk2@gmail.com>
2019-02-07 21:35:46 +0300

    Moved the code in main function in linreg. Also started linreg model using sklearn.

--- LinReg/linreg.py
+++ LinReg/linreg.py
@@ -54,78 +54,85 @@ def trainLinReg(trainInputs, trainTargets, alpha = 0.05, maxEpochs = 1000, minCo
 
     return W
 
-numFolds = 10
-alpha = 12e-10 # for non-standardized
-#alpha = 76e-5 # for standardized
-maxEpochs = 10000
-minCostDiff = 0.0001
-data = readData("../../Datasets/concrete/concrete.csv", delim=",")
+def main():
 
-verbose = True
-normalize = False
+    numFolds = 10
+    alpha = 12e-10 # for non-standardized
+    #alpha = 76e-5 # for standardized
+    maxEpochs = 10000
+    minCostDiff = 0.0001
+    data = readData("../../Datasets/concrete/concrete.csv", delim=",")
 
-# shuffle the data
-tData = data.T
-np.random.shuffle(tData)
-data = tData.T
+    verbose = True
+    normalize = False
 
-avgMSE = 0
+    # shuffle the data
+    tData = data.T
+    np.random.shuffle(tData)
+    data = tData.T
 
-for foldNum in range(numFolds):
+    avgMSE = 0
 
-    print("Fold {}".format(foldNum))
+    for foldNum in range(numFolds):
 
-    # separate data into train and test partitions for the current fold
-    trainData, testData = generateFoldsReg(data, numFolds, foldNum + 1)
+        print("Fold {}".format(foldNum))
 
-    if normalize == True:
-        # normalize data using the statistics of the training data
-        train_mean = np.mean(trainData, axis = 1)
-        train_std = np.std(trainData, axis = 1) + 0.1
-        trainMeanTrainTiled = np.tile(np.array([train_mean]).T, [1, trainData.shape[1]])
-        trainStdTrainTiled = np.tile(np.array([train_std]).T, [1, trainData.shape[1]])
-        trainMeanTestTiled = np.tile(np.array([train_mean]).T, [1, testData.shape[1]])
-        trainStdTestTiled = np.tile(np.array([train_std]).T, [1, testData.shape[1]])
+        # separate data into train and test partitions for the current fold
+        trainData, testData = generateFoldsReg(data, numFolds, foldNum + 1)
+
+        if normalize == True:
+            # normalize data using the statistics of the training data
+            # otherwise, if we normalize the whole dataset at the beginning, data leakage may occur
+            train_mean = np.mean(trainData, axis = 1)
+            train_std = np.std(trainData, axis = 1) + 0.1
+            trainMeanTrainTiled = np.tile(np.array([train_mean]).T, [1, trainData.shape[1]])
+            trainStdTrainTiled = np.tile(np.array([train_std]).T, [1, trainData.shape[1]])
+            trainMeanTestTiled = np.tile(np.array([train_mean]).T, [1, testData.shape[1]])
+            trainStdTestTiled = np.tile(np.array([train_std]).T, [1, testData.shape[1]])
+            
+            trainData = (trainData - trainMeanTrainTiled) / trainStdTrainTiled
+            testData = (testData - trainMeanTestTiled) / trainStdTestTiled
+
+        trainInputs = trainData[0:trainData.shape[0] - 1, :]
+        trainTargets = trainData[trainData.shape[0] - 1, :]
+        testInputs = testData[0:testData.shape[0] - 1, :]
+        testTargets = testData[testData.shape[0] - 1, :]
         
-        trainData = (trainData - trainMeanTrainTiled) / trainStdTrainTiled
-        testData = (testData - trainMeanTestTiled) / trainStdTestTiled
+        # append 1s to features of train inputs and test inputs for the bias term
+        trainInputs = np.vstack((trainInputs, np.ones((trainInputs.shape[1]))))
+        testInputs = np.vstack((testInputs, np.ones((testInputs.shape[1]))))
 
-    trainInputs = trainData[0:trainData.shape[0] - 1, :]
-    trainTargets = trainData[trainData.shape[0] - 1, :]
-    testInputs = testData[0:testData.shape[0] - 1, :]
-    testTargets = testData[testData.shape[0] - 1, :]
+        # train the linear regression model on the train data
+        W = trainLinReg(trainInputs, trainTargets, alpha, maxEpochs, minCostDiff)
+
+        # test the model on test data and calculate MSE
+        zTest = W.T.dot(testInputs)
+        
+        if normalize == True:
+            
+            # un-standardize target values
+            zTest = np.multiply(zTest, train_std[8]) + train_mean[8]
+            testTargets = np.multiply(testTargets, train_std[8]) + train_mean[8]
+            
+        MSETest = np.sum(np.power((zTest - testTargets), 2)) / testInputs.shape[1]
+
+        if verbose == True:
+            
+            sortedTargetIndices = np.argsort(testTargets)
+            plt.scatter(np.arange(testTargets.shape[0]), testTargets[sortedTargetIndices], color="r", label="actual")
+            plt.scatter(np.arange(zTest.shape[1]), zTest[0, sortedTargetIndices], color="b", label="predicted")
+            plt.legend()
+            plt.show()
+
+        print("MSE for fold {} on test data is {}".format(foldNum, MSETest))
+        print()
+
+        avgMSE += MSETest
+
+    avgMSE /= numFolds
+
+    print("Average MSE is {}".format(avgMSE))
+
+if __name__ == "__main__":
     
-    # append 1s to features of train inputs and test inputs for the bias term
-    trainInputs = np.vstack((trainInputs, np.ones((trainInputs.shape[1]))))
-    testInputs = np.vstack((testInputs, np.ones((testInputs.shape[1]))))
-
-    # train the linear regression model on the train data
-    W = trainLinReg(trainInputs, trainTargets, alpha, maxEpochs, minCostDiff)
-
-    # test the model on test data and calculate MSE
-    zTest = W.T.dot(testInputs)
-    
-    if normalize == True:
-        
-        # un-standardize target values
-        zTest = np.multiply(zTest, train_std[8]) + train_mean[8]
-        testTargets = np.multiply(testTargets, train_std[8]) + train_mean[8]
-        
-    MSETest = np.sum(np.power((zTest - testTargets), 2)) / testInputs.shape[1]
-
-    if verbose == True:
-        
-        sortedTargetIndices = np.argsort(testTargets)
-        plt.scatter(np.arange(testTargets.shape[0]), testTargets[sortedTargetIndices], color="r", label="actual")
-        plt.scatter(np.arange(zTest.shape[1]), zTest[0, sortedTargetIndices], color="b", label="predicted")
-        plt.legend()
-        plt.show()
-
-    print("MSE for fold {} on test data is {}".format(foldNum, MSETest))
-    print()
-
-    avgMSE += MSETest
-
-avgMSE /= numFolds
-
-print("Average MSE is {}".format(avgMSE))
+    main()
---
+++ LinReg/linreg_sklearn.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from linreg import readData
+
+def main():
+
+    numFolds = 10
+    
+
+if __name__ == "__main__":
+
+    main()
